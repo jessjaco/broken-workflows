b2eb9cd171eb86c63dbf20b10a3fbd44b065601f
Jesse Anderson <jesse.anderson@gmail.com>
2024-11-11 18:12:50 -0700

    use dask and don't try to get odc to write multiband

--- stac_search_n_load.py
+++ stac_search_n_load.py
@@ -2,12 +2,13 @@ import boto3
 import odc.stac
 from odc.geo.xr import write_cog
 from pystac_client import Client
+from dask.distributed import Client as DDClient
 from typer import run
 
 # roughly the size of a dep tile
 SEARCH_AREA = (148, -6, 149, -5)
 CATALOG = "https://earth-search.aws.element84.com/v1"
-DATETIME = "2020"
+DATETIME = "2021"
 
 
 def main(datetime: str = DATETIME):
@@ -19,13 +20,16 @@ def main(datetime: str = DATETIME):
 
     ds = odc.stac.load(
         items,
-        chunks=dict(band=1, time=1, x=2048, y=2048),
-        bands=["qa_pixel", "nir08", "swir16", "swir22", "red", "blue", "green"],
+        chunks=dict(time=1, x=1024, y=1024),
+        bands=["nir08", "swir16", "swir22", "red", "blue", "green"],
     )
-    median = ds.median(dim="time").to_array("band")
-    write_cog(median, "temp.tif", use_windowed_writes=True)
+    median = ds.median(dim="time")
+    for var in ds:
+        print(var)
+        write_cog(median[var], "temp.tif")
 
 
 if __name__ == "__main__":
     odc.stac.configure_s3_access(cloud_defaults=True, requester_pays=True)
-    run(main)
+    with DDClient():
+        run(main)
